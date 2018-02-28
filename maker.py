a36138ac62cbf5fe9a8bba4f6b7e321056651506
asalo <corvettelover1997@gmail.com>
2018-02-28 01:49:28 -0700

    added a description

--- maker.py
+++ maker.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def find_max(array1, array2):
+def find_max(array1, array2):  # a function to find the max and the 3dB Bandwidth from dB data
     max = -1000
     for i, ele in enumerate(array1):
         if array1[i] > max:
@@ -14,19 +14,18 @@ def find_max(array1, array2):
             maxi = i
     print('Max= ', max)
     print('f= ', array2[maxi])
-    # to find the 3dB BW
-    for j in range(0, maxi):
-        if array1[j] < max - 3:
+    for j in range(0, maxi):  # to find the 3dB BW
+        if array1[j] < max - 3:  # to find lower
             lower = array1[j]
             loweri= j
-    for l in range(maxi, len(array1) - 1):
+    for l in range(maxi, len(array1) - 1):  # to find higher
         if array1[l] > max - 3:
             upper = array1[l]
             upperi = l
     print('BW =', array2[upperi] - array2[loweri])
 
 
-plt.figure(1)
+plt.figure(1)  # Plot 1 and inputing files
 a, b, c, d = np.loadtxt("input/Lab 4 - sheet5.csv", delimiter=',', unpack= True, skiprows= 1)
 e, f = np.loadtxt('input/LAB4Stuff - PhasePlotSeriesBW20000.csv', delimiter=',', unpack=True, skiprows=2, usecols=(1, 2))
 g, h = np.loadtxt('input/LAB4Stuff - PhaseSeriesBW5000.csv', delimiter=',', unpack=True, skiprows=3, usecols=(1, 2))
@@ -36,6 +35,7 @@ x2, y2 = np.loadtxt('input/LAB4Stuff - PhasePlotParallelBW5000.csv', delimiter='
 x3, y3 = np.loadtxt('input/LAB4Stuff - MagnitudePlotParallelBW5000.csv', delimiter=',', unpack=True, skiprows=3, usecols=(1, 2))
 
 
+# add plots to figure 1
 plt.plot(a, b, label='R = 1850')
 plt.plot(a, c, label='R = 3700')
 plt.plot(a, d, label='R = 7400')
@@ -119,36 +119,5 @@ axes = plt.gca()
 axes.set_xlim(0, 60000)
 plt.savefig("output/phaserspara.png")
 
-
-
-# x0, y0 = np.loadtxt("input/tek0026CH1.csv", delimiter=',', unpack= True, skiprows= 21)
-# x1, y1 = np.loadtxt("input/tek0027CH1.csv", delimiter=',', unpack= True, skiprows= 21)
-# x2, y2 = np.loadtxt("input/tek0028CH1.csv", delimiter=',', unpack= True, skiprows= 21)
-# x3, y3 = np.loadtxt("input/tek0029CH1.csv", delimiter=',', unpack= True, skiprows= 21)
-# x4, y4 = np.loadtxt("input/tek0030CH1.csv", delimiter=',', unpack= True, skiprows= 21)
-# x5, y5 = np.loadtxt("input/tek0031CH1.csv", delimiter=',', unpack= True, skiprows= 21)
-# x6, y6 = np.loadtxt("input/tek0032CH1.csv", delimiter=',', unpack= True, skiprows= 21)
-# x7, y7 = np.loadtxt("input/tek0033CH1.csv", delimiter=',', unpack= True, skiprows= 21)
-# x8, y8 = np.loadtxt("input/tek0034CH1.csv", delimiter=',', unpack= True, skiprows= 21)
-# x9, y9 = np.loadtxt("input/tek0035CH2.csv", delimiter=',', unpack= True, skiprows= 21)
-# x10, y10 = np.loadtxt("input/tek0036CH2.csv", delimiter=',', unpack= True, skiprows= 21)
-#
-#
-# plt.figure(4)
-# plt.plot(x0, y0)
-# plt.plot(x1, y1)
-# plt.plot(x2, y2)
-# axes = plt.gca()
-# axes.set_xlim(-.00025, .00025)
-#
-# f, axarr = plt.subplots(8, sharex=True)
-# axarr[0].plot(x3, y3)
-# axarr[1].plot(x4, y4)
-# axarr[2].plot(x5, y5)
-# axarr[3].plot(x6, y6)
-# axarr[4].plot(x7, y7)
-# axarr[5].plot(x8, y8)
-# axarr[6].plot(x9, y9)
-# axarr[7].plot(x10, y10)
 
 plt.show()
